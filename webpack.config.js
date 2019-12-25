3ac8e2b8bd3ec6203d0717073188de2ac809f8c1
hws <hongweisheng77@gmail.com>
2019-12-25 16:56:08 -0500

    add webpack file

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// webpack main config file
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[hash].js',
+        // absolute path
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        progress: true,
+        contentBase: './dist'
+    },
+    plugins: [ // all plugins here
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+            },
+            hash: true,
+        }),
+    ]
+}
